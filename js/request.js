f8c24f506aca253b961e9f5ffe239389fb86487d
HimaniNegi1993 <himaninegi440@gmail.com>
2019-08-20 13:42:12 +0530

    'firstCommit'

---
+++ js/request.js
@@ -0,0 +1,17 @@
+/*-----------------DEMO JS FOR ALL-------------------*/
+
+function a1(){
+	$('#z1').show();
+	$('#z2').hide();
+	$('#z3').hide();
+}
+function a2(){
+	$('#z1').hide();
+	$('#z2').show();
+	$('#z3').hide();
+}
+function a3(){
+	$('#z1').hide();
+	$('#z2').hide();
+	$('#z3').show();
+}
